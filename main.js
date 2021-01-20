490969bca2d47ebea593eba62bce126ccfae26ff
rosalybt <rosalybalbuena.15@gmail.com>
2021-01-20 19:33:14 -0400

    primera parte de los ejercicios

---
+++ main.js
@@ -0,0 +1,75 @@
+//**********PRIMERA PARTE DE LOS EJERCICIOS */
+
+// const alumnas = ["Sol", "Angie", "Flor"]
+// const edades = [1, 23, 75, 33, 111, 12, 101]
+// const alumnasObj = [
+//     {
+//         nombre: "Sol",
+//         edad: 17
+//     }, {
+//         nombre: "Angie",
+//         edad: 52
+//     }, {
+//         nombre: "Flor",
+//         edad: 25
+//     }
+// ]
+
+
+// Utilizando el metodo sort ordenar alfabeticamente 
+// el array alumnas, de menor a mayor el array edades, 
+// y de la edad mas pequeña a la mas grande el array alumnasObj
+
+
+// let copiaAlumnas = [...alumnas]
+
+// copiaAlumnas.sort()
+// console.log(alumnas)
+// console.log(copiaAlumnas)
+
+
+
+// let copiaEdades = [...edades]
+
+// copiaEdades.sort((a, b) => {
+//     if (a > b) {
+//         return 1;
+//     }
+//     if (a < b) {
+//         return -1;
+//     }
+//     // a must be equal to b
+//     return 0;
+// }
+
+// )
+
+// console.log(copiaEdades)
+
+
+// let copiaObjAlumna = [...alumnasObj]
+
+// console.log(copiaObjAlumna)
+// copiaObjAlumna.sort((a, b) => {
+//     if (a.edad > b.edad) {
+//         return 1;
+//     }
+//     else if (a.edad < b.edad) {
+//         return -1;
+//     } else {
+//         return 0;
+//     }
+
+// }
+// )
+// console.log(copiaObjAlumna)
+
+//********** FIN PRIMERA PARTE DE LOS EJERCICIOS*************** */
+
+
+
+
+
+//**********SEGUNDA PARTE DE LOS EJERCICIOS*************** */
+
+
